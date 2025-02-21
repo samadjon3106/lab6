4d9acb7e312caae5257fa62cff94045b9f892e81
samadjon3106 <s.sayfullayev@newuu.uz>
2025-02-21 15:17:22 +0500

    Update Patient.h

--- Patient.h
+++ Patient.h
@@ -12,7 +12,7 @@ private:
     int age;
     string treatment;
     string diagnosis;
-    
+
 public:
     Patient(int ID, string name, int age, string diagnosis ){
         this->ID = ID;
@@ -20,27 +20,31 @@ public:
         this->age = age;
         this->diagnosis = diagnosis;
     }
-    
+
     int getID(){
         return ID;
     }
-    
+
     string getName(){
         return name;
     }
-    
+    string getTreatment() {
+        return treatment;
+    }
+    string getDiagnosis() {
+        return diagnosis;
+    }
     void setName(string name){
         this->name = name;
     }
-    
+
     void setTreatment(string treatment){
         this->treatment = treatment;
     }
-    
+
     void displayInfo(){
         cout << "ID: " << ID << " | Name: " << name << " | Age: " << age << endl;
         cout << "Diagnosis: " << diagnosis << " | Treatment: " << treatment << endl;
     }
 };
 #endif /* Patient_h */
-
